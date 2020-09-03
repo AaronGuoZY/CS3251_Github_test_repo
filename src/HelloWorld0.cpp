1b990050956b1f386950a0aa45620d68b708bcc3
Aaron Guo <zaiyang.guo@vanderbilt.edu>
2020-09-04 01:22:17 +0800

    Greetings from Aaron

---
+++ src/HelloWorld0.cpp
@@ -0,0 +1,24 @@
+// File name: //TODO: Modify File Name
+// Author: Aaron Guo
+// VUnetid: guoz10
+// Email: zaiyang.guo@vanderbilt.edu
+// Class: CS2201
+// Date: //TODO: Enter the date
+// Honor statement: I have neither given nor received any unauthorized aid on this assignment.
+// Assignment Number: //TODO: Enter Assignment Number
+//
+// Description: //TODO: Enter Description
+//
+//
+// Last Changed: //TODO: Update this when necessary
+
+//
+// Created by Aaron (Zaiyang) Guo on 9/4/20.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World from Aaron!" << std::endl;
+    return 0;
+}
